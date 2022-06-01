cf708b9a34f1a12e83d919578b399a52f09bd25b
Djuk1c <djuk1c@protonmail.com>
2022-06-01 02:09:16 +0200

    room data

--- src/world.rs
+++ src/world.rs
@@ -1,14 +1,23 @@
 use crate::*;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 pub struct WorldPlugin;
 impl Plugin for WorldPlugin {
     fn build(&self, app: &mut App) {
         app
-        .add_startup_system(spawn_world);
+        .insert_resource(RoomsData(Vec::new()))
+        .add_startup_system(spawn_world)
+        .add_startup_system(load_rooms.before(spawn_world));
     }
 }
 
+// Components
 #[derive(Component)]
 struct Wall;
+
+// Resources
+struct RoomsData(Vec<Vec<String>>);
 
 fn find_rooms(room_count: u32) -> [[i32; 20]; 20] {
     // Pick random point in rooms_grid
@@ -46,7 +55,17 @@ fn find_rooms(room_count: u32) -> [[i32; 20]; 20] {
     return rooms_grid;
 }
 
-fn spawn_world(mut commands: Commands, mut map: ResMut<Map>) {
+fn load_rooms(mut rooms: ResMut<RoomsData>) {
+    let file = File::open("assets/rooms.dat").unwrap();
+    let reader = BufReader::new(file);
+    for (_, line) in reader.lines().enumerate() {
+        let line = line.unwrap(); // Ignore errors.
+        let line: Vec<String> = line.split_whitespace().map(|s| s.parse().expect("parse error")).collect();
+        rooms.0.push(line);
+    }
+}
+
+fn spawn_world(mut commands: Commands, mut map: ResMut<Map>, rooms: Res<RoomsData>) {
     let rooms_grid = find_rooms(8);
     for (y, row) in rooms_grid.iter().enumerate() {
         for (x, _col) in row.iter().enumerate() {
@@ -77,6 +96,31 @@ fn spawn_world(mut commands: Commands, mut map: ResMut<Map>) {
                             .insert(Wall);
                             map.0[(x as i32 * ROOM_SIZE as i32 + x_room) as usize][(y as i32 * ROOM_SIZE as i32 + y_room) as usize] = 1;
                         }
+                    }
+                }
+                // Draw room data
+                let room_data = rooms.0[rand::thread_rng().gen_range(0, rooms.0.len())].clone();
+                println!("{:?}", room_data);
+                for cell in room_data {
+                    if cell.chars().nth(0).unwrap() == '1' { // It's a wall
+                        let x_room = cell.chars().nth(1).unwrap() as i32 - '0' as i32;      // Hackerman
+                        let y_room = cell.chars().nth(2).unwrap() as i32 - '0' as i32; 
+
+                        commands.spawn_bundle(SpriteBundle {
+                            sprite: Sprite {
+                                color: WALL_COLOR,
+                                ..default()
+                            },
+                            transform: Transform {
+                                translation: Vec3::new((x as f32 * CELL_SIZE * ROOM_SIZE) as f32 + (x_room + 1) as f32 * CELL_SIZE, (-(y as f32) * CELL_SIZE * ROOM_SIZE) as f32 + -((y_room + 1) as i32) as f32 * CELL_SIZE, 0.0),
+                                scale: Vec3::new(CELL_SIZE, CELL_SIZE, CELL_SIZE),
+                                ..default()
+                            },
+                            ..default()
+                        })
+                        .insert(Position((x as i32 + x_room + 1) as u32, (y as i32 + y_room + 1) as u32))
+                        .insert(Wall);
+                        map.0[(x as i32 * ROOM_SIZE as i32 + x_room + 1) as usize][(y as i32 * ROOM_SIZE as i32 + y_room + 1) as usize] = 1;
                     }
                 }
             }
