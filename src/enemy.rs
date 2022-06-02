448b2d0634a615f8f13611ce9d808f34d78604d4
Djuk1c <djuk1c@protonmail.com>
2022-06-02 16:13:07 +0200

    los bug fix

--- src/enemy.rs
+++ src/enemy.rs
@@ -22,11 +22,6 @@ fn enemy_check_los(player: Query<&mut Position, With<Player>>, mut enemies: Quer
     let player = player.single();
     for mut enemy in enemies.iter_mut() {
         let mut wall = false;
-        // View distance
-        if (enemy.0.0 as i32 - player.0 as i32).abs() > view_distance || (enemy.0.1 as i32 - player.1 as i32).abs() > view_distance {
-            enemy.1.color = ENEMY_COLOR;
-            continue
-        }
         let points = get_line(Point2d::new(enemy.0.0, enemy.0.1), Point2d::new(player.0, player.1));
         for point in points {
             if map.0[point.x as usize][point.y as usize] == 1 {
@@ -36,6 +31,10 @@ fn enemy_check_los(player: Query<&mut Position, With<Player>>, mut enemies: Quer
         }
         if !wall {
             enemy.1.color = ENEMY_ALERT_COLOR;
+        }
+        // View distance
+        if (enemy.0.0 as i32 - player.0 as i32).abs() > view_distance || (enemy.0.1 as i32 - player.1 as i32).abs() > view_distance || wall {
+            enemy.1.color = ENEMY_COLOR;
         }
     }
     to_move.0 = false;
